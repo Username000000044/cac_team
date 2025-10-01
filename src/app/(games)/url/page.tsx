f2072cd4dcae47128dbd6a3b84402d45e9ea3d75
000000044 <000000044@student.baisd.net>
2025-10-01 10:23:55 -0400

    url game

--- src/app/(games)/url/page.tsx
+++ src/app/(games)/url/page.tsx
@@ -1,5 +1,16 @@
 export default function URLPage() {
  return(
-    <div className="h-screen w-full bg-red-500"></div>
+    <div className="w-full">
+      <div className="gap-5 mx-auto max-w-[75rem] xl:max-w-[90rem] grid grid-cols-1 grid-rows-2 md:grid-cols-[3fr_1fr] md:grid-rows-1">
+         <div className="flex flex-col gap-5">
+            <div className="h-[80vh] bg-red-500"></div>
+            <div className="h-[20vh] bg-green-500"></div>
+
+         </div>
+
+         <div className="h-[100vh] bg-red-400"></div>
+
+      </div>
+    </div>
  )
 }
