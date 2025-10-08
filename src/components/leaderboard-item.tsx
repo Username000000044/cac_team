ce502404526bf15982920664580618aab2ed0c0e
Username000000044 <willlevi@proton.me>
2025-10-07 21:42:25 -0400

    img

--- src/components/leaderboard-item.tsx
+++ src/components/leaderboard-item.tsx
@@ -10,20 +10,20 @@ interface RankType {
 
 export const LeaderboardItem = ({ rank, name, score, date, img }: RankType) => {
   return (
-    <div className="bg-card h-20 grow mb-5 rounded-lg p-3">
+    <div className="bg-card h-20 min-w-50 grow mb-5 rounded-lg p-3">
       <div className="flex h-full">
         <Image
           width={54}
           height={54}
           src={img + `?username=[${name}]`}
           alt="profile image"
-          className="flex self-center items-center justify-center rounded-full bg-secondary text-secondary-foreground text-2xl"
+          className="flex self-center items-center justify-center rounded-full bg-secondary text-secondary-foreground text-2xl me-4"
         ></Image>
 
-        <div className="flex flex-col items-start justify-center ms-5">
-          <div className="flex justify-between items-center">
-            <p>{name}</p>
-            <p className="ms-2 text-[.8rem] text-muted-foreground">
+        <div className="flex flex-col items-start justify-center">
+          <div className="flex justify-between flex-wrap items-center">
+            <p className="me-2">{name}</p>
+            <p className="text-sm text-muted-foreground">
               {date.getDate() +
                 " / " +
                 date.getMonth() +
@@ -31,7 +31,7 @@ export const LeaderboardItem = ({ rank, name, score, date, img }: RankType) => {
                 date.getFullYear()}
             </p>
           </div>
-          <p className=" text-muted-foreground text-sm">({score})</p>
+          <p className="text-muted-foreground text-sm">({score})</p>
         </div>
       </div>
     </div>
