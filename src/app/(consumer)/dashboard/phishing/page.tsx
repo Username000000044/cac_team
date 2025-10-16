dc101540d59df9bbdca523aeee2b8c9463a0a9e4
Username000000044 <willlevi@proton.me>
2025-10-15 20:00:27 -0400

    subscription

--- src/app/(consumer)/dashboard/phishing/page.tsx
+++ src/app/(consumer)/dashboard/phishing/page.tsx
@@ -4,9 +4,7 @@ export default function PhishingPage() {
   return (
     <div className="flex flex-1">
       <div className="flex flex-wrap justify-center gap-3 w-full text-primary-foreground">
-        <SubscriptionCard header="Starter Shield" price="0.00" />
-        <SubscriptionCard header="Smart Defender" price="2.00" />
-        <SubscriptionCard header="Team Guardian" price="4.00" />
+        <SubscriptionCard card={1} />
       </div>
     </div>
   );
--- src/components/subscription-card.tsx
+++ src/components/subscription-card.tsx
@@ -8,56 +8,65 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+interface Subscription {
+  id: number;
+  header: string;
+  price: string;
+  buttonText: string;
+  features: Array<string>;
+}
 
-const subscriptionOptions = [
-    {
-        header: "Starter Sheld",
-        price: "0.00",
-        buttonText: "Get Starter Shield",
-        varient: "shield",
-        features: [
-            "1 phishing test email per week (randomized style).",
-            "Instant feedback after each test.",
-            "Mini lessons on what to look for (simple tips section).",
-            "Basic progress tracker (percentage of correct responces)."
-        ]
-    },
-    {
-        header: "Smart Defender",
-        price: "2.00",
-        buttonText: "Get Smart Defender",
-        varient: "defender",
-        features: [
-            "Up to 5 phishing test emails per week.",
-            "Progress history (see your last 5 results).",
-            "Custom difficulty mode (easy / medium / hard).",
-            "Certificate of completion every month (PDF download)."
-        ]
-    },
-    {
-        header: "Team Guardian",
-        price: "4.00",
-        buttonText: "Get Team Guardian",
-        varient: "guardian",
-        features: [
-            "Group progress dashboard (shared leaderboard).",
-            "Team-wide phishing challege (weekly 'race to report').",
-            "Custom welcome email for the group",
-            "Priority support"
-        ]
-    },
-    
-]
+const subscription: Subscription[] = [
+  {
+    id: 1,
+    header: "Starter Shield",
+    price: "0.00",
+    buttonText: "Get Starter Shield",
+    features: [
+      "1 phishing test email per week (randomized style).",
+      "Instant feedback after each test.",
+      "Mini lessons on what to look for (simple tips section).",
+      "Basic progress tracker (percentage of correct responces).",
+    ],
+  },
+  {
+    id: 2,
+    header: "Smart Defender",
+    price: "2.00",
+    buttonText: "Get Smart Defender",
+    features: [
+      "Up to 5 phishing test emails per week.",
+      "Progress history (see your last 5 results).",
+      "Custom difficulty mode (easy / medium / hard).",
+      "Certificate of completion every month (PDF download).",
+    ],
+  },
+  {
+    id: 3,
+    header: "Team Guardian",
+    price: "4.00",
+    buttonText: "Get Team Guardian",
+    features: [
+      "Group progress dashboard (shared leaderboard).",
+      "Team-wide phishing challege (weekly 'race to report').",
+      "Custom welcome email for the group",
+      "Priority support",
+    ],
+  },
+];
 
-
-
-export const SubscriptionCard = ({ }: SubscriptionCardTypes) => {
+export const SubscriptionCard = ({ id }: any) => {
   return (
     <Card className="bg-primary/30 border border-primary w-full max-w-100">
       <CardHeader className="flex flex-col items-center">
-        <CardTitle className="text-3xl font-thin">{subscription.header}</CardTitle>
-        <CardDescription className="text-lg"><p>${subscription.price}<span className="text-sm">/mo</span></p></CardDescription>
-        {/* <CardAction>Card Action</CardAction> */}
+        <CardTitle className="text-3xl font-thin">{id.header}</CardTitle>
+        <CardDescription className="text-lg">
+          <p>
+            ${id.price}
+            <span className="text-sm">/mo</span>
+          </p>
+        </CardDescription>
+        <CardAction>Card Action</CardAction>
       </CardHeader>
       <CardContent>
         <p>Card Content</p>
